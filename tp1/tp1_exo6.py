3f991acf8f597cda14bf01b3ebc9ebbcd39eb150
Alessandro-Bauer-Sicari <alessandro68260@gmail.com>
2025-11-06 16:47:12 +0100

    Add files via upload

---
+++ tp1/tp1_exo6.py
@@ -0,0 +1,8 @@
+
+minutes = int(input("Entrez le nombre de minutes écoulées depuis le début du mois : "))
+
+
+jour = minutes // (24 * 60)
+
+
+print("La date correspond au jour", jour, "du mois.")
---
+++ tp1/tp1exo8.py
@@ -0,0 +1,6 @@
+import random
+
+
+valeur = random.randint(0, 100)
+
+print("Nombre aléatoire généré :", valeur)
